a8c4b47925a1b82bb6a876a595438fbb02e48ed3
eliza876 <eliza.cujaban@uptc.edu.co>
2024-11-15 00:26:03 -0500

    COMMIT CHANGES PROPERTIES

--- src/main/java/com/uptc/frw/casoestudio/controller/TrabajadorController.java
+++ src/main/java/com/uptc/frw/casoestudio/controller/TrabajadorController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/trabajadores")
+@RequestMapping("/trabajadores")
 public class TrabajadorController {
 
     @Autowired
